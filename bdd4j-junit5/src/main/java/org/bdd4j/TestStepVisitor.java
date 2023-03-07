203e1fa0b49cc631e861bcdbf6272f67d4ac8f0c
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2023-03-07 16:54:16 +0100

    Moved TestStepVisitor into its own class

---
+++ bdd4j-junit5/src/main/java/org/bdd4j/TestStepVisitor.java
@@ -0,0 +1,80 @@
+package org.bdd4j;
+
+/**
+ * A visitor that can be used to execute step specific logic.
+ *
+ * @param <T> The type of the test state.
+ */
+public final class TestStepVisitor<T> implements StepVisitor<T>
+{
+
+  private TestState<T> state;
+
+  /**
+   * Creates a new instance.
+   *
+   * @param initialState The initial state of the test.
+   */
+  public TestStepVisitor(final TestState<T> initialState)
+  {
+    this.state = initialState;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void visit(final Given<T> step) throws Throwable
+  {
+    if (state.exception() != null)
+    {
+      throw state.exception();
+    }
+
+    try
+    {
+      state = step.logic().apply(state.state());
+    } catch (final Throwable exception)
+    {
+      state = TestState.exception(exception);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void visit(final When<T> step) throws Throwable
+  {
+    if (state.exception() != null)
+    {
+      throw state.exception();
+    }
+
+    try
+    {
+      state = step.logic().apply(state.state());
+    } catch (final Throwable exception)
+    {
+      state = TestState.exception(exception);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void visit(final Then<T> step)
+  {
+    try
+    {
+      state = step.logic().apply(state);
+    } catch (final AssertionError assertionError)
+    {
+      throw assertionError;
+    } catch (final Throwable exception)
+    {
+      state = TestState.exception(exception);
+    }
+  }
+}
