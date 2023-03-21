eb9b3548d5b68a96971d1b59ec1e194c6ce9c692
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2023-03-21 16:05:16 +0100

    Improve javadoc for BDD4jSteps
    https://github.com/bdd4j/bdd4j/issues/13

--- bdd4j-junit5/src/main/java/org/bdd4j/BDD4jSteps.java
+++ bdd4j-junit5/src/main/java/org/bdd4j/BDD4jSteps.java
@@ -1,7 +1,20 @@
 package org.bdd4j;
 
 /**
- * This is an abstract base class that needs to be
+ * This is the common interface that needs to be implemented by classes that provide access to
+ * the steps used in bdd4j scenarios.
+ * <p>
+ * Implementations should define factory methods for the {@link Given}, {@link When} and
+ * {@link Then} steps for the scenarios, by using the builders defined in the {@link StepDSL} class
+ * and exposing them to tests.
+ * <p>
+ * To initialize the test state the interface provides the {@link BDD4jSteps#init()} method, which
+ * must be implemented by concrete step classes.
+ * <p>
+ * Instances of the steps can be injected into the scenario test methods. This is done by the
+ * {@link BDD4jParameterResolver}.
+ *
+ * @param <T> The type of the state managed by the BDD4j test.
  */
 public interface BDD4jSteps<T>
 {
