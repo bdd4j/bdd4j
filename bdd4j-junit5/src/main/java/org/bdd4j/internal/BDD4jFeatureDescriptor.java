56dcd32c9ee18a483aa288b414de45197645df7e
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2024-01-05 09:01:42 +0100

    Added missing javadocs to BDD4jFeatureDescriptor

--- bdd4j-junit5/src/main/java/org/bdd4j/internal/BDD4jFeatureDescriptor.java
+++ bdd4j-junit5/src/main/java/org/bdd4j/internal/BDD4jFeatureDescriptor.java
@@ -3,13 +3,23 @@ package org.bdd4j.internal;
 import org.junit.platform.engine.UniqueId;
 import org.junit.platform.engine.support.descriptor.AbstractTestDescriptor;
 
+/**
+ * A descriptor for a bdd4j feature.
+ */
 public class BDD4jFeatureDescriptor extends AbstractTestDescriptor {
 
   private final Class<?> testClass;
 
+  /**
+   * Creates a new instance.
+   *
+   * @param uniqueId    The unique ID.
+   * @param displayName The display name.
+   * @param testClass   The test class.
+   */
   public BDD4jFeatureDescriptor(final UniqueId uniqueId,
-                                   final String displayName,
-                                   final Class<?> testClass) {
+                                final String displayName,
+                                final Class<?> testClass) {
     super(uniqueId, displayName);
     this.testClass = testClass;
   }
@@ -22,6 +32,11 @@ public class BDD4jFeatureDescriptor extends AbstractTestDescriptor {
     return Type.CONTAINER;
   }
 
+  /**
+   * The test class that is represented by the descriptor.
+   *
+   * @return The test class.
+   */
   public Class<?> getTestClass() {
     return testClass;
   }
