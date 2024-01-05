cf89dd44f2119dd99932fea202123e27e42f20ab
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2024-01-05 08:58:00 +0100

    Removed unused code

--- bdd4j-junit5/src/main/java/org/bdd4j/internal/BDD4jEngineExecutionContext.java
+++
@@ -1,6 +0,0 @@
-package org.bdd4j.internal;
-
-import org.junit.platform.engine.support.hierarchical.EngineExecutionContext;
-
-public class BDD4jEngineExecutionContext implements EngineExecutionContext {
-}
